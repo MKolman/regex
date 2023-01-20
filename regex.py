c752945659218bdf5892b5d9562f8e2636b61658
Maks Kolman <maks.kolman@bitstamp.net>
2023-01-20 14:18:56 +0100

    Complete literal and add wildcard

--- regex.py
+++ regex.py
@@ -1,38 +1,88 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import typing
+
+
+class Node(typing.Protocol):
+    def match(self, haystack: str, idx: int) -> bool:
+        ...
+
+
+class Empty:
+    def match(self, haystack: str, idx: int) -> bool:
+        return True
+
+
+@dataclass
+class Wildcard:
+    success: typing.Optional[Node] = None
+
+    def match(self, haystack: str, idx: int) -> bool:
+        if idx >= len(haystack):
+            return False
+        if self.success is None:
+            return True
+        return self.success.match(haystack, idx + 1)
 
 
 @dataclass
 class Literal:
-    jumps: dict[str, 'Literal'] = field(default_factory=dict)
-    is_final: bool = False
+    char: str
+    fail: typing.Optional[Node] = None
+    success: typing.Optional[Node] = None
 
-    def match(self, haystack: str, idx: int, fallback: typing.Optional['Literal'] = None) -> bool:
-        if self.is_final:
-            return True
+    def match(self, haystack: str, idx: int) -> bool:
         if idx >= len(haystack):
             return False
-        fallback = fallback or self
-        return self.jumps.get(haystack[idx], fallback).match(haystack, idx + 1, fallback)
+        if haystack[idx] == self.char:
+            if self.success is None:
+                return True
+            return self.success.match(haystack, idx + 1)
+        elif self.fail is not None:
+            return self.fail.match(haystack, idx)
+        else:
+            return self.match(haystack, idx + 1)
 
-    @staticmethod
-    def builder(needle: str) -> 'Literal':
-        cur = Literal({}, True)
-        for c in reversed(needle):
-            cur = Literal({c: cur})
-        return cur
 
-def self_similar(s:str, i:int) -> int:
-    """
-    n = self_similar(s, i)
-    s[i-n:i] == s[:n]
-    """
-    pass
+def fail_table(needle: str) -> list[typing.Optional[int]]:
+    """https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm#Description_of_pseudocode_for_the_table-building_algorithm"""
+    pos = 1
+    candidate = 0
+    jump_table = [None] * len(needle)
+    for pos, current in enumerate(needle[1:], 1):
+        if current == needle[candidate]:
+            jump_table[pos] = jump_table[candidate]
+        else:
+            jump_table[pos] = candidate
+            while candidate is not None and current != needle[candidate]:
+                candidate = jump_table[candidate]
+        candidate = 0 if candidate is None else candidate + 1
+    return jump_table
+
 
 class Regex:
     def __init__(self, needle):
         self.needle = needle
-        self.state_machine = Literal.builder(needle)
+        self.state_machine = self._builder(needle)
 
     def match(self, haystack):
         return self.state_machine.match(haystack, 0)
+
+    @staticmethod
+    def _builder(needle: str) -> Node:
+        if not needle:
+            return Empty()
+
+        fail_idxs = fail_table(needle)
+        if needle[0] == ".":
+            start = Wildcard()
+        else:
+            start = Literal(needle[0])
+        state: list[Node] = [start]
+        for fail_idx, c in zip(fail_idxs[1:], needle[1:]):
+            if c == ".":
+                state.append(Wildcard())
+            else:
+                fail = start if fail_idx is None else state[fail_idx]
+                state.append(Literal(c, fail))
+            state[-2].success = state[-1]
+        return start
--- test_regex.py
+++ test_regex.py
@@ -1,6 +1,6 @@
 import unittest
 
-from regex import Literal, Regex
+from regex import Literal, Regex, fail_table
 
 
 class TestBasic(unittest.TestCase):
@@ -20,21 +20,59 @@ class TestBasic(unittest.TestCase):
         self.assertTrue(re.match("jsdhfgasdf"))
         self.assertTrue(re.match("asdfsdjkh"))
         self.assertTrue(re.match("mdsbasdfsdjkh"))
-    
+
     def test_self_similar(self):
         re = Regex("ab")
         self.assertTrue(re.match("aab"))
 
 
-class TestLiteral(unittest.TestCase):
-
+class TestBuilder(unittest.TestCase):
     def test_match(self):
-        l = Literal({"a": Literal({"b": Literal({}, True)})})
-        self.assertTrue(l.match("ab", 0))
-
-    def test_match_final(self):
-        self.assertTrue(Literal({}, True))
+        re = Regex._builder("ab")
+        self.assertTrue(re.match("ab", 0))
 
     def test_builder(self):
-        re = Literal.builder("asdf")
+        re = Regex._builder("asdf")
         self.assertTrue(re.match("asdf", 0))
+        self.assertFalse(re.match("asf", 0))
+
+
+class TestFailTable(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual([], fail_table(""))
+
+    def test_simple(self):
+        self.assertEqual([None, None, 1], fail_table("aad"))
+
+    def test_medium(self):
+        self.assertEqual([None, 0, None, 0, 2], fail_table("ababc"))
+
+
+class TestWildcard(unittest.TestCase):
+    def test_match(self):
+        re = Regex("..")
+        self.assertTrue(re.match("abc"))
+        self.assertFalse(re.match(""))
+        self.assertFalse(re.match("a"))
+
+        re = Regex("a.")
+        self.assertTrue(re.match("abc"))
+        self.assertTrue(re.match("baba"))
+        self.assertFalse(re.match("a"))
+        self.assertFalse(re.match("ca"))
+
+
+class TestOptional(unittest.TestCase):
+    def test_simple(self):
+        re = Regex("ba?b")
+        self.assertTrue(re.match("bb"))
+        self.assertTrue(re.match("bab"))
+        self.assertFalse(re.match("baab"))
+        self.assertFalse(re.match("b"))
+
+    def test_wtf(self):
+        re = Regex("ba?a")
+        self.assertTrue(re.match("ba"))
+        self.assertTrue(re.match("baa"))
+        self.assertFalse(re.match("b"))
+        self.assertFalse(re.match("aa"))
